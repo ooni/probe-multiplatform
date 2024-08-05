b0d2f99416dc3f48bf599972e6f8fd56d3b443ca
Sérgio Santos <me@sergiosantos.info>
2024-08-05 11:07:38 +0100

    Reorder TaskSettings attributes by required/optional

--- composeApp/src/commonMain/kotlin/org/ooni/engine/models/TaskSettings.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/engine/models/TaskSettings.kt
@@ -5,18 +5,20 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TaskSettings(
+    // Required
     @SerialName("name") val name: String,
     @SerialName("inputs") val inputs: List<String>,
     @SerialName("version") val version: Int = 1,
     @SerialName("log_level") val logLevel: TaskLogLevel,
     @SerialName("disabled_events") val disabledEvents: List<String> = emptyList(),
+    @SerialName("options") val options: Options,
+    @SerialName("annotations") val annotations: Annotations,
+    // Optional
     @SerialName("proxy") val proxy: String? = null,
     @SerialName("state_dir") val stateDir: String? = null,
     @SerialName("temp_dir") val tempDir: String? = null,
     @SerialName("tunnel_dir") val tunnelDir: String? = null,
     @SerialName("assets_dir") val assetsDir: String? = null,
-    @SerialName("options") val options: Options,
-    @SerialName("annotations") val annotations: Annotations,
 ) {
     @Serializable
     data class Options(
