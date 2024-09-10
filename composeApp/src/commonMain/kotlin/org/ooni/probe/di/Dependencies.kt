fe3e181ea6fa3b1b0ef5cd9ca38064bee14fc042
Sérgio Santos <me@sergiosantos.info>
2024-09-10 16:18:37 +0100

    Fix formatting

--- composeApp/src/commonMain/kotlin/org/ooni/probe/di/Dependencies.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/di/Dependencies.kt
@@ -190,6 +190,7 @@ class Dependencies(
             configureAutoRun = configureAutoRun,
         )
     }
+
     private fun runNetTest(spec: RunNetTest.Specification) =
         RunNetTest(
             startTest = engine::startTask,
