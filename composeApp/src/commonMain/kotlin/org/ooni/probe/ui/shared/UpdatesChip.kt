01c8ff603f2ccea4c711e8025e43aaf20f891ac3
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-10-02 16:39:09 +0100

    chore: change updated chip

--- composeApp/src/commonMain/kotlin/org/ooni/probe/ui/shared/UpdatesChip.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/ui/shared/UpdatesChip.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import ooniprobe.composeapp.generated.resources.Dashboard_RunV2_UpdatedTag
+import ooniprobe.composeapp.generated.resources.Dashboard_RunV2_UpdateTag
 import ooniprobe.composeapp.generated.resources.Res
 import org.jetbrains.compose.resources.stringResource
 
@@ -13,9 +13,10 @@ import org.jetbrains.compose.resources.stringResource
 fun UpdatesChip(onClick: () -> Unit) {
     SuggestionChip(
         onClick = onClick,
+        enabled = false,
         colors = SuggestionChipDefaults.suggestionChipColors(
             labelColor = MaterialTheme.colorScheme.error,
         ),
-        label = { Text(stringResource(Res.string.Dashboard_RunV2_UpdatedTag)) },
+        label = { Text(stringResource(Res.string.Dashboard_RunV2_UpdateTag)) },
     )
 }
