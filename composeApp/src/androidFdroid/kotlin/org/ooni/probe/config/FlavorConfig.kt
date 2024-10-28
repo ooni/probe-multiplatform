284fa7c7a412d74941935f88881f968e4fd0dcf8
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-10-28 12:18:44 +0100

    chore: update based on review

--- composeApp/src/androidFdroid/kotlin/org/ooni/probe/config/FlavorConfig.kt
+++ composeApp/src/androidFdroid/kotlin/org/ooni/probe/config/FlavorConfig.kt
@@ -3,8 +3,4 @@ package org.ooni.probe.config
 class FlavorConfig : FlavorConfigInterface {
     override val optionalFeatures: Set<OptionalFeature>
         get() = emptySet()
-    override val isCrashReportingEnabled: Boolean
-        get() = optionalFeatures.contains(OptionalFeature.CrashReporting)
-    override val isRemoteNotificationsEnabled: Boolean
-        get() = optionalFeatures.contains(OptionalFeature.RemoteNotifications)
 }
