7888e422332a17cc20a5a8e284ba821af998b7ce
Norbel Ambanumben <aanorbel@gmail.com>
2025-01-14 20:09:43 +0100

    revert to prod apis

--- composeApp/src/commonMain/release/kotlin/org/ooni/probe/config/BuildTypeDefaults.kt
+++ composeApp/src/commonMain/release/kotlin/org/ooni/probe/config/BuildTypeDefaults.kt
@@ -1,8 +1,8 @@
 package org.ooni.probe.config
 
 object BuildTypeDefaults : BuildTypeDefaultsInterface {
-    override val ooniApiBaseUrl = "https://api.dev.ooni.io"
-    override val ooniRunDomain = "run.test.ooni.org"
+    override val ooniApiBaseUrl = "https://api.ooni.org"
+    override val ooniRunDomain = "run.ooni.org"
     override val ooniRunDashboardUrl = "https://run.ooni.org"
     override val explorerUrl = "https://explorer.ooni.org"
 }
