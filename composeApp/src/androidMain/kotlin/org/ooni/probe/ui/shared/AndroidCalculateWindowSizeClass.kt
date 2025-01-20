ccf4f3a164b74a06fea977837cd95b4a39d5c78c
Norbel Ambanumben <aanorbel@gmail.com>
2025-01-20 09:45:05 +0100

    chore: fix ci error

--- composeApp/src/androidMain/kotlin/org/ooni/probe/ui/shared/AndroidCalculateWindowSizeClass.kt
+++ composeApp/src/androidMain/kotlin/org/ooni/probe/ui/shared/AndroidCalculateWindowSizeClass.kt
@@ -1,10 +1,10 @@
 package org.ooni.probe.ui.shared
 
 import android.app.Activity
+import androidx.activity.compose.LocalActivity
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
-actual fun calculateWindowSizeClass(): WindowSizeClass = calculateWindowSizeClass(LocalContext.current as Activity)
+actual fun calculateWindowSizeClass(): WindowSizeClass = calculateWindowSizeClass(LocalActivity.current as Activity)
