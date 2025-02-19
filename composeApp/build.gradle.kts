e46ed57df820c6c1dd5774ea49bde3f5d00a03cb
Norbel Ambanumben <aanorbel@gmail.com>
2025-02-19 11:48:20 +0100

    chore: increment version code for release

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -154,7 +154,7 @@ android {
         applicationId = config.appId
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 150 // Always increment by 10. See fdroid flavor below
+        versionCode = 151 // Always increment by 10. See fdroid flavor below
         versionName = "5.0.3"
         resValue("string", "app_name", config.appName)
         resValue("string", "ooni_run_enabled", config.supportsOoniRun.toString())
