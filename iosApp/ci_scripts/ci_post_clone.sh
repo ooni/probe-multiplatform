613c769359255be58989d3a11ec0f3d08842b2a8
Norbel Ambanumben <aanorbel@gmail.com>
2025-03-03 21:34:56 +0100

    chore: update post_clone script

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -87,7 +87,7 @@ export JAVA_HOME="${jdk_dir}/Home"
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-echo "organization=${$ORGANIZATION}" >> gradle.properties
+echo "organization=${ORGANIZATION}" >> gradle.properties
 
 ./gradlew copyBrandingToCommonResources -Porganization=$ORGANIZATION
 
