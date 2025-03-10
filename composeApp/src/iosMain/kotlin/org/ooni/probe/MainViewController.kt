32124a03a3d7edc81c5db3093e5c8a8f806a3c59
Norbel Ambanumben <aanorbel@gmail.com>
2025-03-10 18:58:30 +0100

    chore: start ios automated screenshots

--- composeApp/src/iosMain/kotlin/org/ooni/probe/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/ooni/probe/MainViewController.kt
@@ -1,8 +1,10 @@
 package org.ooni.probe
 
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 import kotlinx.coroutines.flow.MutableSharedFlow
 import org.ooni.probe.data.models.DeepLink
@@ -10,11 +12,14 @@ import org.ooni.probe.di.Dependencies
 import org.ooni.probe.ui.shared.requestAppReview
 import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalComposeApi::class)
 fun mainViewController(
     dependencies: Dependencies,
     deepLinkFlow: MutableSharedFlow<DeepLink?>,
 ): UIViewController {
-    return ComposeUIViewController {
+    return ComposeUIViewController (configure = {
+        accessibilitySyncOptions = AccessibilitySyncOptions.Always(debugLogger = null)
+    }) {
         val deepLink by deepLinkFlow.collectAsState(null)
         App(
             dependencies = dependencies,
