3df5e5021ab7f755430267d9ad08402e4318ff25
Norbel AMBANUMBEN <norbel@openobservatory.org>
2025-03-31 14:20:39 +0100

    feat: integrate `auto-launch` functionality and add tray menu (#626)
    * feat: integrate auto-launch functionality and add tray menu
    
    * chore: add an appropriate tray icon

--- composeApp/src/desktopMain/kotlin/org/ooni/probe/Main.kt
+++ composeApp/src/desktopMain/kotlin/org/ooni/probe/Main.kt
@@ -1,15 +1,49 @@
 package org.ooni.probe
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.github.vinceglb.autolaunch.AutoLaunch
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import ooniprobe.composeapp.generated.resources.Res
 import ooniprobe.composeapp.generated.resources.app_name
+import ooniprobe.composeapp.generated.resources.tray_icon
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 fun main() {
     application {
+        val autoLaunch = AutoLaunch(appPackageName = "org.openobservatory.ooniprobe")
+
+        var isWindowVisible by remember { mutableStateOf(!autoLaunch.isStartedViaAutostart()) }
+
+        // start an hourly background task that calls startSingleRun
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1000 * 60 * 60)
+                startSingleRun()
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            autoLaunch.enable()
+        }
+
         Window(
-            onCloseRequest = ::exitApplication,
+            onCloseRequest = {
+                isWindowVisible = false
+            },
+            visible = isWindowVisible,
+            icon = painterResource(Res.drawable.tray_icon),
             title = stringResource(Res.string.app_name),
         ) {
             App(
@@ -18,5 +52,33 @@ fun main() {
                 onDeeplinkHandled = {},
             )
         }
+
+        Tray(
+            icon = painterResource(Res.drawable.tray_icon),
+            tooltip = stringResource(Res.string.app_name),
+            menu = {
+                Item(
+                    "Show App",
+                    onClick = {
+                        isWindowVisible = !isWindowVisible
+                    },
+                )
+                Item(
+                    "Run Test",
+                    onClick = ::startSingleRun,
+                )
+                Separator()
+                Item(
+                    "Exit",
+                    onClick = ::exitApplication,
+                )
+            },
+        )
+    }
+}
+
+private fun startSingleRun() {
+    CoroutineScope(Dispatchers.IO).launch {
+        dependencies.runBackgroundTask(null).collect()
     }
 }
