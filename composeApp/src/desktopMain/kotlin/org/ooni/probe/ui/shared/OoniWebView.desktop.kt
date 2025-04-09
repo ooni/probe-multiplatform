c1c698b5b18b5c2c543379b6f36fac2762e70c99
Sérgio Santos <me@sergiosantos.info>
2025-04-09 15:49:40 +0100

    JavaFX webview PoC

--- composeApp/src/desktopMain/kotlin/org/ooni/probe/ui/shared/OoniWebView.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/ooni/probe/ui/shared/OoniWebView.desktop.kt
@@ -1,7 +1,16 @@
 package org.ooni.probe.ui.shared
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
+import javafx.embed.swing.JFXPanel
+import javafx.application.Platform
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
+import javafx.scene.web.WebView
 
 @Composable
 actual fun OoniWebView(
@@ -9,4 +18,34 @@ actual fun OoniWebView(
     modifier: Modifier,
     allowedDomains: List<String>,
 ) {
+    val onCreated = {}
+    val onDispose = {}
+    val currentOnDispose by rememberUpdatedState(onDispose)
+
+    DisposableEffect(Unit) {
+        onDispose {
+            currentOnDispose()
+        }
+    }
+
+    SwingPanel(
+        factory = {
+            JFXPanel().apply {
+                Platform.runLater {
+                    val webView = WebView().apply {
+                        isVisible = true
+                        // engine.addLoadListener(state, navigator)
+                        engine.isJavaScriptEnabled = true
+                    }
+                    val root = StackPane()
+                    root.children.add(webView)
+                    this.scene = Scene(root)
+                    // state.webView = DesktopWebView(webView)
+                    onCreated()
+                    webView.engine.load("https://ooni.org")
+                }
+            }
+        },
+        modifier = modifier,
+    )
 }
