954a385c7126d3763fdfee4de390abe614d80d80
Norbel Ambanumben <aanorbel@gmail.com>
2025-06-11 18:17:01 +0100

    feat: add support for more connection types on android

--- composeApp/src/androidMain/kotlin/org/ooni/engine/AndroidNetworkTypeFinder.kt
+++ composeApp/src/androidMain/kotlin/org/ooni/engine/AndroidNetworkTypeFinder.kt
@@ -18,6 +18,9 @@ class AndroidNetworkTypeFinder(
             capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> NetworkType.VPN
             capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> NetworkType.Wifi
             capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> NetworkType.Mobile
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> NetworkType.Unknown("wired_ethernet")
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> NetworkType.Unknown("bluetooth")
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_USB) -> NetworkType.Unknown("usb")
             else -> NetworkType.NoInternet
         }
     }
