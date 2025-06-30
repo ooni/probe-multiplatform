36d94e8a11199434ad1f93300d1dce4a3ae14cd3
Sérgio Santos <me@sergiosantos.info>
2025-06-30 13:05:52 +0100

    Fix compose previews

--- composeApp/src/androidMain/kotlin/org/ooni/probe/ui/shared/CalculateWindowSizeClass.android.kt
+++ composeApp/src/androidMain/kotlin/org/ooni/probe/ui/shared/CalculateWindowSizeClass.android.kt
@@ -1,10 +1,15 @@
 package org.ooni.probe.ui.shared
 
-import android.app.Activity
 import androidx.activity.compose.LocalActivity
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 
 @Composable
-actual fun calculateWindowSizeClass(): WindowSizeClass = calculateWindowSizeClass(LocalActivity.current as Activity)
+actual fun calculateWindowSizeClass(): WindowSizeClass =
+    LocalActivity.current
+        ?.let { calculateWindowSizeClass(it) }
+        // Generic default size
+        ?: WindowSizeClass.calculateFromSize(DpSize(360.dp, 800.dp))
