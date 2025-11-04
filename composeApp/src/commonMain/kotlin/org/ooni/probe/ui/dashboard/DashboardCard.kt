7032827e63addefef5d8cbd1dd64c8facc1ee4e2
Sérgio Santos <me@sergiosantos.info>
2025-11-04 16:56:02 +0000

    Open articles on external browser

--- composeApp/src/commonMain/kotlin/org/ooni/probe/ui/dashboard/DashboardCard.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/ui/dashboard/DashboardCard.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
