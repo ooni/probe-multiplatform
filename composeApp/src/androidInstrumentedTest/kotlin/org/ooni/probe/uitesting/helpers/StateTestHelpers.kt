8f7db06cd16f210510e33b8b1b0902746283fa2f
Norbel Ambanumben <aanorbel@gmail.com>
2025-11-18 09:11:19 +0100

    chore: optimize last check time calculation for GeoIP DB updates

--- composeApp/src/androidInstrumentedTest/kotlin/org/ooni/probe/uitesting/helpers/StateTestHelpers.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/org/ooni/probe/uitesting/helpers/StateTestHelpers.kt
@@ -2,6 +2,7 @@ package org.ooni.probe.uitesting.helpers
 
 import org.ooni.probe.data.models.SettingsKey
 import org.ooni.probe.domain.organizationPreferenceDefaults
+import kotlin.time.Clock
 
 suspend fun skipOnboarding() {
     preferences.setValueByKey(SettingsKey.FIRST_RUN, false)
@@ -14,6 +15,7 @@ suspend fun defaultSettings() {
             SettingsKey.AUTOMATED_TESTING_ENABLED to true,
             SettingsKey.AUTOMATED_TESTING_WIFIONLY to true,
             SettingsKey.AUTOMATED_TESTING_CHARGING to true,
+            SettingsKey.MMDB_LAST_CHECK to Clock.System.now().toEpochMilliseconds(),
         ) +
             organizationPreferenceDefaults(),
     )
