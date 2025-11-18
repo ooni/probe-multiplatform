2ed164509e44dad3a17bb01a3226b22b85450014
Sérgio Santos <me@sergiosantos.info>
2025-11-18 14:07:48 +0000

    Update dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -131,6 +131,7 @@ kotlin {
                 optIn("kotlin.io.encoding.ExperimentalEncodingApi")
                 optIn("kotlin.time.ExperimentalTime")
                 optIn("kotlinx.cinterop.ExperimentalForeignApi")
+                optIn("kotlinx.coroutines.DelicateCoroutinesApi")
                 optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
                 optIn("kotlinx.coroutines.FlowPreview")
                 optIn("org.jetbrains.compose.resources.ExperimentalResourceApi")
