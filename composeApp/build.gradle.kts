917bebf3ea8ac7cfd3dc61c54ecab162936415cf
Sérgio Santos <me@sergiosantos.info>
2025-12-15 10:34:38 +0000

    Update dependencies (#1026)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,7 +64,7 @@ kotlin {
 
         // See https://github.com/getsentry/sentry-kotlin-multiplatform?tab=readme-ov-file#cocoa-sdk-version-compatibility-table
         pod("Sentry") {
-            version = "8.57.1"
+            version = "8.57.3"
             extraOpts += listOf("-compiler-option", "-fmodules")
         }
 
