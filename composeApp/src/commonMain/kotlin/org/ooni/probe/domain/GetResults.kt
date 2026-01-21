3479adf31ccf0da36202d4053b1e9c6f730037a3
Norbel Ambanumben <aanorbel@gmail.com>
2026-01-21 17:57:30 +0100

    chore: updare result matching

--- composeApp/src/commonMain/kotlin/org/ooni/probe/domain/GetResults.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/domain/GetResults.kt
@@ -35,27 +35,11 @@ class GetResults(
         }
 }
 
-private fun matchesDescriptor(
-    item: DescriptorItem,
-    result: ResultModel,
-): Boolean {
-    val key = result.descriptorKey
-    // A non-default descriptor matches if its key ID is the same as the result's descriptor key.
-    val isNonDefaultMatch = !item.isDefault() && item.descriptor.key.id == key
-    // A default descriptor matches if its test name corresponds to the result's descriptor name.
-    val isDefaultMatch = item.isDefault() && OoniTest.fromId(item.descriptor.key.id.value)?.key == result.descriptorName
-
-    return isNonDefaultMatch || isDefaultMatch
-}
-
 fun List<DescriptorItem>.forResult(result: ResultModel?): DescriptorItem? =
     result
-        ?.let { result ->
-            firstOrNull {
-                matchesDescriptor(it, result)
-            }
-        }
-        ?: firstOrNull { it.descriptor.name == result?.descriptorName }
+        ?.descriptorKey
+        ?.let { descriptorKey -> firstOrNull { descriptorKey == it.descriptor.key } }
+        ?: firstOrNull { OoniTest.fromId(it.descriptor.id.value)?.key == result?.descriptorName }
 
 fun List<TestKeysWithResultId>.forResult(result: ResultModel): List<TestKeysWithResultId>? =
     result.id
