cc5c1b498c11825030be2308f2f17ea3140acb5a
Sérgio Santos <me@sergiosantos.info>
2026-03-04 16:53:27 +0000

    Submit with credentials

--- composeApp/src/commonMain/kotlin/org/ooni/passport/PassportBridge.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/passport/PassportBridge.kt
@@ -1,30 +1,35 @@
 package org.ooni.passport
 
 import org.ooni.engine.models.Result
+import org.ooni.passport.PassportBridge.KeyValue
 import org.ooni.passport.models.CredentialResponse
 import org.ooni.passport.models.PassportException
 import org.ooni.passport.models.PassportHttpResponse
 
-interface PassportBridge {
-    fun get(
+interface PassportBridge :
+    PassportGet,
+    PassportAuthRegister,
+    PassportAuthSubmit,
+    PassportPost {
+    override fun get(
         url: String,
-        headers: List<KeyValue> = emptyList(),
-        query: List<KeyValue> = emptyList(),
+        headers: List<KeyValue>,
+        query: List<KeyValue>,
     ): Result<PassportHttpResponse, PassportException>
 
-    fun post(
+    override fun post(
         url: String,
-        headers: List<KeyValue> = emptyList(),
+        headers: List<KeyValue>,
         payload: String,
     ): Result<PassportHttpResponse, PassportException>
 
-    fun userAuthRegister(
+    override fun userAuthRegister(
         url: String,
         publicParams: String,
         manifestVersion: String,
     ): Result<CredentialResponse, PassportException>
 
-    fun userAuthSubmit(
+    override fun userAuthSubmit(
         url: String,
         credential: String,
         publicParams: String,
@@ -38,4 +43,40 @@ interface PassportBridge {
         val key: String,
         val value: String,
     )
+}
+
+fun interface PassportGet {
+    fun get(
+        url: String,
+        headers: List<KeyValue>,
+        query: List<KeyValue>,
+    ): Result<PassportHttpResponse, PassportException>
+}
+
+fun interface PassportPost {
+    fun post(
+        url: String,
+        headers: List<KeyValue>,
+        payload: String,
+    ): Result<PassportHttpResponse, PassportException>
+}
+
+fun interface PassportAuthRegister {
+    fun userAuthRegister(
+        url: String,
+        publicParams: String,
+        manifestVersion: String,
+    ): Result<CredentialResponse, PassportException>
+}
+
+fun interface PassportAuthSubmit {
+    fun userAuthSubmit(
+        url: String,
+        credential: String,
+        publicParams: String,
+        content: String,
+        probeCc: String,
+        probeAsn: String,
+        manifestVersion: String,
+    ): Result<CredentialResponse, PassportException>
 }
--- composeApp/src/commonMain/kotlin/org/ooni/probe/domain/credentials/CredentialsConstants.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/domain/credentials/CredentialsConstants.kt
@@ -1,5 +1,5 @@
 package org.ooni.probe.domain.credentials
 
 object CredentialsConstants {
-    const val CREDENTIALS_KEY = "credentials"
+    const val STORAGE_KEY = "credential"
 }
