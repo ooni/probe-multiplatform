4397015a91c18dd95a5a274fcafca6c22d5ca39e
Norbel Ambanumben <aanorbel@gmail.com>
2026-03-09 13:59:25 +0100

    chore: update passport dependency to use Maven coordinates

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -115,7 +115,7 @@ kotlin {
                 }
 
                 implementation("org.ooni:oonimkall:c52ce3b5-${oonimkallVersionSuffix()}")
-                implementation(files("libs/passport-${passportDependencySuffix()}.jar"))
+                implementation("org.ooni:passport-${passportDependencySuffix()}:e56538fd")
             }
         }
         // Testing
@@ -321,7 +321,9 @@ android {
         debugImplementation(libs.androidx.ui.tooling.preview)
         "fullImplementation"(libs.bundles.full.android)
         "fullImplementation"("org.ooni:oonimkall:3.29.0-android:@aar")
+        "fullImplementation"("org.ooni:passport-android:e56538fd")
         "fdroidImplementation"("org.ooni:oonimkall:3.29.0-android:@aar")
+        "fdroidImplementation"("org.ooni:passport-android:e56538fd")
         "xperimentalImplementation"(files("libs/android-oonimkall.aar"))
         implementation(files("libs/passport-android.aar"))
         androidTestUtil(libs.android.orchestrator)
