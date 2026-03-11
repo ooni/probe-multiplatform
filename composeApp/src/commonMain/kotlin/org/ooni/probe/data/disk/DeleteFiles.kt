22fd211f5afbeb4dadea7c427812012f18682bd6
Sérgio Santos <me@sergiosantos.info>
2026-03-11 11:20:54 +0000

    Fix DeleteFiles crash

--- composeApp/src/commonMain/kotlin/org/ooni/probe/data/disk/DeleteFiles.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/data/disk/DeleteFiles.kt
@@ -1,8 +1,12 @@
 package org.ooni.probe.data.disk
 
+import co.touchlab.kermit.Logger
+import kotlinx.coroutines.withContext
 import okio.FileSystem
+import okio.IOException
 import okio.Path
 import okio.Path.Companion.toPath
+import kotlin.coroutines.CoroutineContext
 
 fun interface DeleteFiles {
     suspend operator fun invoke(path: Path)
@@ -11,8 +15,15 @@ fun interface DeleteFiles {
 class DeleteFilesOkio(
     private val fileSystem: FileSystem,
     private val baseFilesDir: String,
+    private val backgroundContext: CoroutineContext,
 ) : DeleteFiles {
     override suspend fun invoke(path: Path) {
-        fileSystem.deleteRecursively(baseFilesDir.toPath().resolve(path))
+        withContext(backgroundContext) {
+            try {
+                fileSystem.deleteRecursively(baseFilesDir.toPath().resolve(path))
+            } catch (e: IOException) {
+                Logger.v("Could not delete files at $path", e)
+            }
+        }
     }
 }
