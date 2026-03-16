8053f31406329243ab71636e51af25a4e222da37
Norbel Ambanumben <aanorbel@gmail.com>
2026-03-16 12:14:46 +0100

    chore: update based on review

--- composeApp/src/commonMain/kotlin/org/ooni/probe/domain/credentials/SetCredential.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/domain/credentials/SetCredential.kt
@@ -1,20 +1,18 @@
 package org.ooni.probe.domain.credentials
 
 import co.touchlab.kermit.Logger
+import kotlinx.serialization.json.Json
 import org.ooni.engine.WriteResult
-import org.ooni.probe.data.models.SettingsKey
+import org.ooni.probe.data.models.Credential
 
 class SetCredential(
     private val writeSecureStorage: suspend (String, String) -> WriteResult,
-    private val setPreference: suspend (SettingsKey, Any?) -> Unit,
+    private val json: Json,
 ) {
-    suspend operator fun invoke(
-        credential: String,
-        emissionDay: UInt,
-    ): Boolean =
+    suspend operator fun invoke(credential: Credential): Boolean =
         try {
-            writeSecureStorage(CredentialsConstants.STORAGE_KEY, credential)
-            setPreference(SettingsKey.EMISSION_DAY, emissionDay.toInt())
+            val bodyText = json.encodeToString(Credential.serializer(), credential)
+            writeSecureStorage(CredentialsConstants.STORAGE_KEY, bodyText)
             Logger.i("User registered successfully, credentials stored")
             true
         } catch (e: Exception) {
