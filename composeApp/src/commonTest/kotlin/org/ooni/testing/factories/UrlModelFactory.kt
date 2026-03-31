904641c44048e52d674f48595bafdac7240a7a4b
Sérgio Santos <me@sergiosantos.info>
2026-03-31 17:35:06 +0100

    Fallback to local URLs if check-in fails

--- composeApp/src/commonTest/kotlin/org/ooni/testing/factories/UrlModelFactory.kt
+++ composeApp/src/commonTest/kotlin/org/ooni/testing/factories/UrlModelFactory.kt
@@ -2,12 +2,12 @@ package org.ooni.testing.factories
 
 import org.ooni.engine.models.WebConnectivityCategory
 import org.ooni.probe.data.models.UrlModel
-import kotlin.random.Random
+import kotlin.uuid.Uuid
 
 object UrlModelFactory {
     fun build(
         id: UrlModel.Id? = null,
-        url: String = "https://ooni.org?random=${Random.nextLong()}",
+        url: String = "https://ooni.org?${Uuid.random()}",
         category: WebConnectivityCategory = WebConnectivityCategory.MISC,
         countryCode: String? = null,
     ) = UrlModel(
