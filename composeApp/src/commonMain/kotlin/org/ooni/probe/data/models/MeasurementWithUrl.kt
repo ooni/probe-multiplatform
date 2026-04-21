b93ba1d12b716ca4eb74d68c6581e766545ef9e7
Sérgio Santos <me@sergiosantos.info>
2026-04-21 17:54:48 +0100

    Refactor measurement url and improve locale string

--- composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/MeasurementWithUrl.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/MeasurementWithUrl.kt
@@ -1,6 +1,30 @@
 package org.ooni.probe.data.models
 
+import androidx.compose.ui.text.intl.Locale
+import io.ktor.http.URLBuilder
+import io.ktor.http.appendPathSegments
+import org.ooni.probe.config.OrganizationConfig
+import org.ooni.probe.shared.languageRegionString
+
 data class MeasurementWithUrl(
     val measurement: MeasurementModel,
     val url: UrlModel?,
-)
+) {
+    val webViewUrl: String?
+        get() {
+            val webViewUrl = URLBuilder(OrganizationConfig.explorerUrl)
+            if (measurement.uid != null && measurement.uid.value.isNotBlank()) {
+                webViewUrl.appendPathSegments(listOf("m", measurement.uid.value))
+            } else if (measurement.reportId != null) {
+                webViewUrl.appendPathSegments(listOf("measurement", measurement.reportId.value))
+                url?.url?.let {
+                    webViewUrl.parameters.append("input", it)
+                }
+            } else {
+                return null
+            }
+            webViewUrl.parameters.append("webview", "true")
+            webViewUrl.parameters.append("language", Locale.current.languageRegionString)
+            return webViewUrl.build().toString()
+        }
+}
