5006287e57c7d87365dbf09124817437f5f680a0
Norbel Ambanumben <aanorbel@gmail.com>
2026-08-10 10:48:48 +0100

    fix: failing ci and formatting

--- composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/NetTest.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/NetTest.kt
@@ -13,23 +13,25 @@ data class NetTest(
     val isBackgroundRunEnabled: Boolean = false,
     val isManualRunEnabled: Boolean = false,
 ) {
-    fun toOONI() = OONINetTest(
-        name = test.name,
-        inputs = inputs,
-        inputsExtra = inputsExtra,
-        targetsName = targetsName,
-        isBackgroundRunEnabled = isBackgroundRunEnabled,
-        isManualRunEnabled = isManualRunEnabled,
-    )
+    fun toOONI() =
+        OONINetTest(
+            name = test.name,
+            inputs = inputs,
+            inputsExtra = inputsExtra,
+            targetsName = targetsName,
+            isBackgroundRunEnabled = isBackgroundRunEnabled,
+            isManualRunEnabled = isManualRunEnabled,
+        )
 
     companion object {
-        fun fromOONI(netTest: OONINetTest) = NetTest(
-            test = TestType.fromName(netTest.name),
-            inputs = netTest.inputs,
-            inputsExtra = netTest.inputsExtra,
-            targetsName = netTest.targetsName,
-            isBackgroundRunEnabled = netTest.isBackgroundRunEnabled,
-            isManualRunEnabled = netTest.isManualRunEnabled,
-        )
+        fun fromOONI(netTest: OONINetTest) =
+            NetTest(
+                test = TestType.fromName(netTest.name),
+                inputs = netTest.inputs,
+                inputsExtra = netTest.inputsExtra,
+                targetsName = netTest.targetsName,
+                isBackgroundRunEnabled = netTest.isBackgroundRunEnabled,
+                isManualRunEnabled = netTest.isManualRunEnabled,
+            )
     }
 }
