850447ada497136d931c0697e16cca758dd142fa
Sérgio Santos <me@sergiosantos.info>
2026-08-17 18:40:45 +0100

    Fix results missing after midnight

--- composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/ResultFilter.kt
+++ composeApp/src/commonMain/kotlin/org/ooni/probe/data/models/ResultFilter.kt
@@ -1,8 +1,10 @@
 package org.ooni.probe.data.models
 
 import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.minus
+import kotlinx.datetime.plus
 import org.ooni.engine.models.TaskOrigin
 import org.ooni.probe.shared.today
 
@@ -22,26 +24,28 @@ data class ResultFilter(
             (if (dates == ResultFilter().dates) 0 else 1)
 
     sealed class Date(
-        val range: ClosedRange<LocalDate>,
+        val range: () -> ClosedRange<LocalDate>,
     ) {
         data object AnyDate :
-            Date(LocalDate(2000, 1, 1)..LocalDate.today())
+            Date(range = { MIN_DATE..MAX_DATE })
 
         data object Today :
-            Date(LocalDate.today()..LocalDate.today())
+            Date(range = { LocalDate.today()..MAX_DATE })
 
         data object FromSevenDaysAgo :
-            Date(LocalDate.today().minus(DatePeriod(days = 7))..LocalDate.today())
+            Date(range = { LocalDate.today().minus(DatePeriod(days = 7))..MAX_DATE })
 
         data object FromOneMonthAgo :
-            Date(LocalDate.today().minus(DatePeriod(months = 1))..LocalDate.today())
+            Date(range = { LocalDate.today().minus(DatePeriod(months = 1))..MAX_DATE })
 
         data class Custom(
             val customRange: ClosedRange<LocalDate>,
-        ) : Date(customRange)
+        ) : Date({ customRange })
     }
 
     companion object {
         const val LIMIT = 100L
+        private val MIN_DATE = LocalDate(2000, 1, 1)
+        private val MAX_DATE = LocalDate.today().plus(1, DateTimeUnit.YEAR)
     }
 }
